61cab280501d05a786f7a0a9d73cf10298bae792
Gilmar Silva Santos Junior <gilmar.santos@jchebly.com.br>
2022-09-11 21:03:55 -0300

    send changes to remote repository

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Post {
+
+    private static $instance = null;
+    
+    private function __construct(){
+        //Torna impossível a criação direta de uma instancia de Post;
+    }
+
+    public static function getInstance(){
+        if(self::$instance == null){
+            self::$instance = new Post();
+        }
+        return self::$instance;
+
+    }
+    public static function testSingletonPattern($comment){
+        echo $comment;
+    }
+}
+
+$singletonInstance = Post::getInstance();
+$singletonInstance::testSingletonPattern('The singleton Pattern Works fine!')
+?>
